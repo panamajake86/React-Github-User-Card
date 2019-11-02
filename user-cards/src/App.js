e13bc708d491ea5c83f0acac75219b00ca9b6ca2
Jake <panamajake86@gmail.com>
2019-11-01 22:17:46 -0600

    got the basic framework down. Got the api call working.  Just need to figure out how to get set it to state.

--- user-cards/src/App.js
+++ user-cards/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: [],
+    usersText: ''
+  };
+
+  componentDidMount() {
+    axios
+    .get('https://api.github.com/users/panamajake86')
+      .then(res => {
+        console.log(res);
+        this.setState({
+          users: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/* <img src={} alt={} /> */}
+        <h1>Name: {this.state.name}</h1>
+        <h3>Username: {this.state.login}</h3>
+        <p>Location: {this.state.location}</p>
+        <p>Profile: {this.state.html_url}</p>
+        <p>Followers: {this.state.followers}</p>
+        <p>Following: {this.state.following}</p>
+        <p>Bio: {this.state.bio}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
